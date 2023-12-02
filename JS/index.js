a370d683a9b278f0cbb14a72d258dfcf97ad2628
Whitney W <131135324+wwaeckerle@users.noreply.github.com>
2023-12-01 21:11:51 -0600

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -70,21 +70,21 @@ $(document).ready(function() {
         const winPatterns = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
             [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-            [0, 4, 8], [2, 4, 6]             // Diagonals
+            [0, 4, 8], [2, 4, 6]// Diagonals
         ];
 
         for (const pattern of winPatterns) {
             const [a, b, c] = pattern;
             if (board[a] && board[a] === board[b] && board[a] === board[c]) {
                 gameOver = true;
-                updateMessage(`${currentPlayer} wins!`);
+                updateMessage(`${currentPlayer} Winner Winner Chicken Dinner!`);
                 return;
             }
         }
 
         if (!board.includes('') && !gameOver) {
             gameOver = true;
-            updateMessage('It\'s a draw!');
+            updateMessage('Chicken Scratch!');
         }
     }
 
