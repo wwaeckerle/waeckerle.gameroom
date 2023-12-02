1e4976e658309db80df37731bc7a06d27be7daba
Whitney W <131135324+wwaeckerle@users.noreply.github.com>
2023-12-01 21:57:56 -0600

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -24,30 +24,3 @@ $(document).ready(function() {
         }
     });
 });
-
-
-//Rock Paper Scissors Game
-$(document).ready(function() {
-    const choices = ['rock', 'paper', 'scissors'];
-
-    $('#choices img').on('click', function() {
-        const userChoice = $(this).attr('alt');
-        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-
-        $('#user').text(userChoice);
-        $('#computer').text(computerChoice);
-
-        // Determine the winner
-        if (userChoice === computerChoice) {
-            $('#result').text('It\'s a tie!');
-        } else if (
-            (userChoice === 'rock' && computerChoice === 'scissors') ||
-            (userChoice === 'paper' && computerChoice === 'rock') ||
-            (userChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-            $('#result').text('You win!');
-        } else {
-            $('#result').text('Computer wins!');
-        }
-    });
-});
